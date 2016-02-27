5050fb490c6b1fcffc42bbeb076860776a5351ed
zyc <z35305515@gmail.com>
2016-02-28 02:56:53 +0800

    async

--- test.js
+++ test.js
@@ -2,17 +2,23 @@
 * @Author: zyc
 * @Date:   2016-02-18 14:39:14
 * @Last Modified by:   zyc
-* @Last Modified time: 2016-02-24 16:28:09
+* @Last Modified time: 2016-02-28 02:55:14
 */
 'use strict'
 
 const client = require('./index')
 
-const book = new client.Book()
-// console.log(book.findOne('熊猫'))
+// const book = new client.Book()
+// book.findOne('熊猫')
+// .then(res => console.log(res))
+// .catch(err => console.error(err))
 
-const movie = new client.Movie()
-// console.log(movie.findOne('熊猫'))
+// const movie = new client.Movie()
+// movie.findOne('熊猫')
+// .then(res => console.log(res))
+// .catch(err => console.error(err))
 
 const music = new client.Music()
-console.log(music.findOne('熊猫'))
+music.findOne('熊猫')
+.then(res => console.log(res))
+.catch(err => console.error(err))
