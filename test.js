99f9acb2a22d5435b6ce4c245070d0f66713005b
zyc <z35305515@gmail.com>
2016-02-28 13:18:15 +0800

    Cannot read property 'length' of undefined

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 * @Author: zyc
 * @Date:   2016-02-18 14:39:14
 * @Last Modified by:   zyc
-* @Last Modified time: 2016-02-28 02:55:14
+* @Last Modified time: 2016-02-28 13:17:41
 */
 'use strict'
 
@@ -13,12 +13,12 @@ const client = require('./index')
 // .then(res => console.log(res))
 // .catch(err => console.error(err))
 
-// const movie = new client.Movie()
-// movie.findOne('熊猫')
-// .then(res => console.log(res))
-// .catch(err => console.error(err))
-
-const music = new client.Music()
-music.findOne('熊猫')
+const movie = new client.Movie()
+movie.findOne('熊猫')
 .then(res => console.log(res))
 .catch(err => console.error(err))
+
+// const music = new client.Music()
+// music.findOne('熊猫')
+// .then(res => console.log(res))
+// .catch(err => console.error(err))
